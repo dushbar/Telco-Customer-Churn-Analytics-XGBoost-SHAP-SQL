81b3d1cc5775a0b8ee66cb757163f7065067b38e
Dushyant Barot <dushbar@users.noreply.github.com>
2026-05-22 19:05:52 +0530

    Add files via upload

---
+++ sql/01_database_schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE TelcoChurnAnalytics;
+GO
+
+USE TelcoChurnAnalytics;
+GO
+
+--create Schema
+CREATE SCHEMA staging;--for raw imported data
+GO
+
+CREATE SCHEMA analytics;--for cleaned analytical views
+GO
